0a71d151c13d60c42f0f79a8af3ed91e526eccdd
Jens Breitbart <jbreitbart@gmail.com>
2012-08-02 22:57:18 +0200

    More comments

--- src/de/kaffeeshare/server/exception/IllegalNamespaceException.java
+++ src/de/kaffeeshare/server/exception/IllegalNamespaceException.java
@@ -1,5 +1,8 @@
 package de.kaffeeshare.server.exception;
 
+/**
+ * Thrown if a reserved namespace name is not legal 
+ */
 public class IllegalNamespaceException extends RuntimeException {
 	
 	private static final long serialVersionUID = -3633614066774348135L;
--- src/de/kaffeeshare/server/exception/ReservedNamespaceException.java
+++ src/de/kaffeeshare/server/exception/ReservedNamespaceException.java
@@ -1,5 +1,8 @@
 package de.kaffeeshare.server.exception;
 
+/**
+ * Thrown if a reserved namespace is supposed to be used
+ */
 public class ReservedNamespaceException extends RuntimeException {
 
 	private static final long serialVersionUID = 2826030527124116551L;

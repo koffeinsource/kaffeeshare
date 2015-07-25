7bd9464aa3945f08c1e7ef1933c7b0a061193f86
Jens Breitbart <jbreitbart@gmail.com>
2015-07-25 15:31:30 +0200

    Updated imgur plugin.

--- extract/imgur.go
+++ extract/imgur.go
@@ -2,6 +2,7 @@ package extract
 
 import (
 	"fmt"
+	"log"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -10,35 +11,29 @@ import (
 )
 
 func imgurl(i *data.Item, sourceURL string, doc *goquery.Document) {
-	fmt.Println("imgurl! " + sourceURL)
 	if !strings.Contains(sourceURL, "imgur.com/") {
 		return
 	}
 
 	fmt.Println("Running imgurl plugin.")
 
-	// update title
-	selection := doc.Find(".image-container")
+	selection := doc.Find("meta[property*='og']")
+
 	if selection.Length() != 0 {
-		i.Description += " - ALBUM"
-	}
-
-	found := 0
-	// get all img within divs with class image
-	doc.Find("div.image img").Each(func(unneeded int, s *goquery.Selection) {
-		m := htmlAttributeToMap(selection.Nodes[0].Attr)
-
-		if govalidator.IsRequestURL(m["src"]) {
-			i.Description = "<img src =\""
-			i.Description += m["src"]
-			i.Description += "\" /> <br/>"
-		} else {
-			fmt.Println("imgurl plugin invalid url. " + m["src"])
-		}
 		i.ImageURL = ""
-	})
-	if found == 0 {
-		fmt.Println("imgurl plugin found no div.image. " + sourceURL)
-	}
+		i.Description = ""
+		for _, e := range selection.Nodes {
+			m := htmlAttributeToMap(e.Attr)
 
+			if m["property"] == "og:image" {
+				if !govalidator.IsRequestURL(m["content"]) {
+					log.Println("Invalid url in og:image. " + sourceURL)
+					continue
+				}
+				i.Description += "<img src =\""
+				i.Description += m["content"]
+				i.Description += "\" /><br/>"
+			}
+		}
+	}
 }

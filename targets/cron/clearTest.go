6b6f1d13926328c52560c184e7ee8f62d69c8b94
Jens Breitbart <jbreitbart@gmail.com>
2016-07-16 15:44:22 +0200

    Updated cron job that clears the test namespace to use our new search index clear.

--- targets/cron/clearTest.go
+++ targets/cron/clearTest.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/koffeinsource/kaffeeshare/data"
-	"google.golang.org/appengine/search"
+	"github.com/koffeinsource/kaffeeshare/search"
 )
 
 // ClearTest deletes all items in the test namespace
@@ -21,25 +21,12 @@ func ClearTest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO clear search index!
-	index, err := search.Open("items_" + namespace)
+	search.ClearSearchItemIndex(con, "test")
 	if err != nil {
-		con.Log.Errorf("Error while opening search index item_%v: %v", namespace, err)
+		con.Log.Errorf("Error while deleting items from search index item_%v: %v", namespace, err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	for t := index.List(con.C, nil); ; {
-		id, err := t.Next(nil)
-		if err == search.Done {
-			break
-		}
-		if err != nil {
-			con.Log.Errorf("Error while deleting items from search index item_%v: %v", namespace, err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		index.Delete(con.C, id)
-	}
 	w.WriteHeader(http.StatusOK)
 }

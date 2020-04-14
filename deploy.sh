fe67e7830903ee1f2404d71d3005420a50f698f9
Jens Breitbart <jbreitbart@gmail.com>
2020-04-14 15:51:34 +0200

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,9 @@
 gcloud app deploy app.yaml
 gcloud app deploy queue.yaml
 gcloud app deploy cron.yaml
+gcloud datastore indexes create index.yaml
+
+#gcloud app deploy app.yaml --project=kaffeeshare
+#gcloud app deploy queue.yaml --project=kaffeeshare
+#gcloud app deploy cron.yaml --project=kaffeeshare
+#gcloud datastore indexes create index.yaml --project=kaffeeshare
